dec70f4cd665a02d20dd13448db190ac7d85c656
tk5cdt <30490920+tk5cdt@users.noreply.github.com>
2024-05-30 22:49:17 +0700

    adjust tablayout

--- app/src/main/java/didong/ungdungchat/Activity/MainActivity.java
+++ app/src/main/java/didong/ungdungchat/Activity/MainActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
 
+import com.google.android.material.tabs.TabLayout;
 import com.google.android.material.tabs.TabLayoutMediator;
 
 import java.util.Objects;
@@ -22,23 +23,36 @@ public class MainActivity extends AppCompatActivity {
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
         setSupportActionBar(binding.mainAppBar);
-        Objects.requireNonNull(getSupportActionBar()).setTitle("Ứng dụng chat");
         binding.mainTabsPager.setAdapter(new TabsAccessorAdapter(this));
         new TabLayoutMediator(binding.mainTabs, binding.mainTabsPager, (tab, position) -> {
             switch (position){
                 case 0:
                     tab.setText("Tin nhắn");
-                    getSupportActionBar().setTitle("Tin nhắn");
+                    Objects.requireNonNull(getSupportActionBar()).setTitle("Tin nhắn");
                     break;
                 case 1:
                     tab.setText("Nhóm");
-                    getSupportActionBar().setTitle("Nhóm");
                     break;
                 case 2:
                     tab.setText("Liên hệ");
-                    getSupportActionBar().setTitle("Liên hệ");
                     break;
             }
         }).attach();
+        binding.mainTabs.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                Objects.requireNonNull(getSupportActionBar()).setTitle(tab.getText());
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+
+            }
+        });
     }
 }
