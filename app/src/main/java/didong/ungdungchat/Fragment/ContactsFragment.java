2c06d524e0fdeb9a53e6356733b62c6d91c033bf
bpbn <bpbngoc11@gmail.com>
2024-06-03 13:14:31 +0700

    add Contacts

--- app/src/main/java/didong/ungdungchat/Fragment/ContactsFragment.java
+++ app/src/main/java/didong/ungdungchat/Fragment/ContactsFragment.java
@@ -4,13 +4,31 @@ package didong.ungdungchat.Fragment;
 
 import android.os.Bundle;
 
+import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
+import com.firebase.ui.database.FirebaseRecyclerAdapter;
+import com.firebase.ui.database.FirebaseRecyclerOptions;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.time.temporal.Temporal;
+
+import de.hdodenhof.circleimageview.CircleImageView;
+import didong.ungdungchat.Model.Contacts;
 import didong.ungdungchat.R;
+import didong.ungdungchat.databinding.FragmentContactsBinding;
+import didong.ungdungchat.databinding.UsersDisplayLayoutBinding;
+
 
 /**
  * A simple {@link Fragment} subclass.
@@ -27,6 +45,14 @@ public class ContactsFragment extends Fragment {
     // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
+
+    FragmentContactsBinding binding;
+
+    private DatabaseReference ContactsRef;
+
+    private FirebaseAuth mAuth;
+
+    private String currentUserID;
 
     public ContactsFragment() {
         // Required empty public constructor
@@ -53,6 +79,7 @@ public class ContactsFragment extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         if (getArguments() != null) {
             mParam1 = getArguments().getString(ARG_PARAM1);
             mParam2 = getArguments().getString(ARG_PARAM2);
@@ -63,6 +90,34 @@ public class ContactsFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_contacts, container, false);
+        View view = inflater.inflate(R.layout.fragment_contacts, container, false);
+        binding = FragmentContactsBinding.bind(view);
+        binding.contactsList.setLayoutManager(new LinearLayoutManager(getContext()));
+
+        mAuth = FirebaseAuth.getInstance();
+        currentUserID = mAuth.getCurrentUser().getUid();
+        ContactsRef = FirebaseDatabase.getInstance().getReference().child("Contacts").child(currentUserID);
+        return view;
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        FirebaseRecyclerOptions options = new FirebaseRecyclerOptions.Builder<Contacts>()
+                .setQuery(ContactsRef, Contacts.class)
+                .build();
+
+        FirebaseRecyclerAdapter<Contacts>
+    }
+    public static class ContactsViewHolder extends RecyclerView.ViewHolder
+    {
+        CircleImageView profileImage;
+        TextView userName, userStatus;
+        public ContactsViewHolder(UsersDisplayLayoutBinding itemView) {
+            super(itemView.getRoot());
+            profileImage = itemView.usersProfileImage;
+            userName = itemView.userProfileName;
+            userStatus = itemView.userStatus;
+        }
     }
 }
